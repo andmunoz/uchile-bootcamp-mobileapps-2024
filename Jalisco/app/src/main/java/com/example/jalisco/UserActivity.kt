87bd3b9ff07554a154c7446ef86e2fe61cfe7aa8
Andrés Muñoz O <andmunoz@gmail.com>
2024-09-24 16:43:19 -0300

    Jalisco example without validations

---
+++ Jalisco/app/src/main/java/com/example/jalisco/UserActivity.kt
@@ -0,0 +1,31 @@
+package com.example.jalisco
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun nextButtonClicked(view: View) {
+        val nameEditText = findViewById<EditText>(R.id.yourNameEditText)
+        val name = nameEditText.text.toString()
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("name", name)
+        startActivity(intent)
+    }
+}
