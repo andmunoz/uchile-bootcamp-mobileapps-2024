c1549eb07141ac07b97edae68e7bc2d6dbe1f049
Andrés Muñoz O <andmunoz@gmail.com>
2024-10-30 20:05:41 -0300

    Class 33 Fragment Example Update

--- FragmentExample/app/src/main/java/com/example/fragmentexample/BlankFragmentB.kt
+++ FragmentExample/app/src/main/java/com/example/fragmentexample/BlankFragmentB.kt
@@ -1,31 +1,48 @@
 package com.example.fragmentexample
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM = "message"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BlankFragmentB.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BlankFragmentB : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param: String) =
+            BlankFragmentB().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM, param)
+                }
+            }
+    }
+    private var message: String? = null
+
+    interface OnMessageSendListener {
+        fun onMessageSend(message: String)
+    }
+    private var messageListener: OnMessageSendListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnMessageSendListener) {
+            messageListener = context
+        } else {
+            throw RuntimeException("$context debe implementar OnMessageSendListener")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            message = it.getString(ARG_PARAM)
         }
     }
 
@@ -37,23 +54,19 @@ class BlankFragmentB : Fragment() {
         return inflater.inflate(R.layout.fragment_blank_b, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BlankFragmentB.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BlankFragmentB().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.fragment_b_textview).text = message
+
+        view.findViewById<View>(R.id.fragment_b_send_button).setOnClickListener {
+            onClickButtonSend(view)
+        }
+    }
+
+    fun onClickButtonSend(view: View) {
+        val messageTextView = view.findViewById<TextView>(R.id.fragment_b_message_box)
+        val message = messageTextView.text.toString()
+        messageListener?.onMessageSend(message)
     }
 }
