3349c3114f6d961f9ba34c832368afb20f4b38ad
Andrés Muñoz O <andmunoz@gmail.com>
2024-11-08 02:33:45 -0300

    Class 35 service example update for gradle 9 configure implementations

--- ServicesExample/app/build.gradle.kts
+++ ServicesExample/app/build.gradle.kts
@@ -43,8 +43,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.gson)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
